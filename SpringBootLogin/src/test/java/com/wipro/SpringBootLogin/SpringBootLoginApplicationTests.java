357cf0be7eb4f000ad39b2e452daa594e2ff759d
Tanmay Tiwari <tanmaytwr01@gmail.com>
2020-06-21 21:12:07 +0530

    Created new project with Spring Initialzr

---
+++ SpringBootLogin/src/test/java/com/wipro/SpringBootLogin/SpringBootLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.SpringBootLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
